42894a44d7b91810a51d3e1f2612597ef1b80571
Erykson Xavier <erykson.soares@gmail.com>
2024-12-24 17:19:42 -0300

    add config and scripts

---
+++ scripts/nginx_status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Definindo as variáveis para salvar os arquivos de log no diretório /var/log/nginx/
+LOG_ONLINE="/var/log/nginx/status_online.log"
+LOG_OFFLINE="/var/log/nginx/status_offline.log"
+
+#Armazenamento da data e hora atual
+DATA=$(date "+%Y-%m-%d %H:%M:$S")
+
+#Variável do nome do serviço
+SERVICO="NGINX"
+
+#Verifica o status do serviço e armazena dentro do arquivo log
+if systemctl is-active --quiet nginx; then
+	echo "$DATA : [$SERVICO - Online] Serviço em execução." >> $LOG_ONLINE
+	echo "NGINX está online"
+else
+	echo "$DATA : [$SERVICO - Offline] Serviçao fora de execução" >> $LOG_OFFLINE
+	echo "NGINX está offline"
+fi
+
